8ce5f069c14bf2a289a63389abdf10c4de7f5085
Ramón Salado <ramonsalado@gmail.com>
2021-01-27 12:15:29 +0100

    Update myini.sh

--- myini.sh
+++ myini.sh
@@ -9,10 +9,10 @@
 ### END INIT INFO
 
 #Clavo el script ini
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 touch /var/lib/dpkg/status
 #Carga de los repo
-echo "deb http://ubuntu.uc3m.es/ubuntu/ xenial main" >> etc/apt/sources.list
+echo "deb http://ubuntu.uc3m.es/ubuntu/ xenial main" > etc/apt/sources.list
 
 #Creador script
 touch /home/myini.sh
@@ -23,3 +23,19 @@ apt-key add ubuntu-archive-keyring.gpg
 apt-get update
 echo "Repositorios Actualizados!"
 EOF
+
+#Modificacion archivo ssh_config con SED
+sed -i "s/#Port 22/Port 2094/g" /etc/ssh/sshd_config
+sed -i "s/#LoginGraceTime 2m/LoginGraceTime 2m/g" /etc/ssh/sshd_config
+sed -i "s/#PermitRootLogin/PermitRootLogin no" /etc/ssh/sshd_config
+sed -i "s/#StrictModes yes/StrictModes yes/g" /etc/ssh/sshd_config
+sed -i "s/#MaxAuthTries 6/MaxAuthTries 3/g" /etc/ssh/sshd_config
+sed -i "s/#MaxSessions 10/MaxSessions 3/g" /etc/ssh/sshd_config
+sed -i "s/X11Forwarding yes/X11Forwarding no/g" /etc/ssh/sshd_config
+
+#Configuracion de IpTables
+iptables -I INPUT -p tcp --dport 22 -j DROP
+iptables -I INPUT -p tcp --dport 2049 -j ACCEPT
+
+#Reiniciamos SSH
+systemctl restart sshd
